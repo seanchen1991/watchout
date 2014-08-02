137184e81febdcfc5b31d4648e2fd750811bd84c
seanchen1991 <skypemaster007@gmail.com>
2014-08-02 13:23:46 -0700

    Added a background image and css stylesheet to update the look.

--- watchout.js
+++ watchout.js
@@ -3,9 +3,10 @@
 var dataset = [], i = 0;
 
 var gameOptions = {
-  height: 450,
-  width: 700,
+  height: $(window).height(),
+  width: $(window).width(),
   nEnemies: 15,
+  speed: 2,
   padding: 20
 };
 
@@ -21,7 +22,8 @@ var axes = {
 var gameBoard = d3.select(".container")
                   .append("svg")
                   .attr("width", gameOptions.width)
-                  .attr("height", gameOptions.height);
+                  .attr("height", gameOptions.height)
+                  .style("fill", "black");
 
 gameStats = {
   score: 0,
@@ -53,7 +55,8 @@ setInterval(increaseScore, 50);
 var enemies = gameBoard.selectAll("circle")
                        .data(dataset)
                        .enter().append("circle")
-                       .style("stroke", "black")
+                       .style("stroke", "green")
+                       .style("stroke-width", 1.5)
                        .style("fill", "black")
                        .attr("cx", function(d, i) { return Math.random() * gameOptions.width; })
                        .attr("cy", function(d, i) { return Math.random() * gameOptions.height; })
@@ -75,17 +78,25 @@ setInterval(function() {
 // player stuff
 
 var Player = function() {
-  this.path = "m-7.5,1.62413c0,-5.04095 4.08318,-9.12413 9.12414,-9.12413c5.04096,0 9.70345,5.53145 11.87586,9.12413c-2.02759,2.72372 -6.8349,9.12415 -11.87586,9.12415c-5.04096,0 -9.12414,-4.08318 -9.12414,-9.12415z";
+  this.path = "M 9  1"
+        + "L 17 26"
+        + "L 9  22"
+        + "L 1  26"
+        + "L 9  1";
   this.x = 0;
   this.y = 0;
   this.angle = 0;
-  this.fill = "red";
+  this.stroke = "green";
+  this["stroke-width"] = 2;
+  this.fill = "black";
   this.r = 10;
 };
 
 Player.prototype.render = function(location) {
   this.el = location.append("svg:path")
-                    .attr('d', this.path)
+                    .attr("d", this.path)
+                    .attr("stroke", this.stroke)
+                    .attr("stroke-width", this["stroke-width"])
                     .attr("fill", this.fill)
   this.transform({
     x: gameOptions.width * .5,
